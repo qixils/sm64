a07e1119216de93917e4186b64ebdaff4ee727a1
Lexi Larkin <noellekiq@gmail.com>
2024-02-21 20:23:04 -0500

    feat: reduce save dialogue

--- text/us/dialogs.h
+++ text/us/dialogs.h
@@ -208,17 +208,10 @@ Would you like to Save?\n\
 //Yes////No"))
 
 DEFINE_DIALOG(DIALOG_013, 1, 5, 30, 200, _("\
-You've collected 100\n\
-coins! Mario gains more\n\
-power from the castle.\n\
 Do you want to Save?\n\
-//Yes////No"))
+//You Bet//Not Now"))
 
 DEFINE_DIALOG(DIALOG_014, 1, 4, 30, 200, _("\
-Wow! Another Power Star!\n\
-Mario gains more courage\n\
-from the power of the\n\
-castle.\n\
 Do you want to Save?\n\
 \n\
 //You Bet//Not Now"))
